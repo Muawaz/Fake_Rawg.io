59a24f9d137e036a14ac058b82298c9ef95e12f4
Muawaz Saleem <muawazsaleem@gmail.com>
2024-09-14 12:09:12 +0500

    Setup routing

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,31 +1,26 @@
-import { Box, Flex, Grid, GridItem, Show } from "@chakra-ui/react";
-import GameGrid from "./components/GameGrid";
-import GameHeading from "./components/GameHeading";
-import GenreList from "./components/GenreList";
-import NavBar from "./components/NavBar";
-import PlatformSelector from "./components/PlatformSelector";
-import SortSelector from "./components/SortSelector";
-import useGameQueryStore from "./store";
+import { Box, Flex, Grid, GridItem, Show } from '@chakra-ui/react';
+import GameGrid from '../components/GameGrid';
+import GameHeading from '../components/GameHeading';
+import GenreList from '../components/GenreList';
+import PlatformSelector from '../components/PlatformSelector';
+import SortSelector from '../components/SortSelector';
+import useGameQueryStore from '../store';
 
-function App() {
-  const { gameQuery, setGenreId, setPlatformId, setSortOrder } =
+const HomePage = () => {
+    const { gameQuery, setGenreId, setPlatformId, setSortOrder } =
     useGameQueryStore();
 
   return (
     <Grid
       templateAreas={{
-        base: `"nav" "main"`,
-        lg: `"nav nav" "aside main"`,
+        base: `"main"`,
+        lg: `"aside main"`,
       }}
       templateColumns={{
         base: "1fr",
         lg: "200px 1fr",
       }}
     >
-      <GridItem area="nav">
-        <NavBar />
-      </GridItem>
-
       <Show above="lg">
         <GridItem area="aside" paddingX={5} /*bg="gold"*/>
           <GenreList />
@@ -48,4 +43,4 @@ function App() {
   );
 }
 
-export default App;
+export default HomePage
